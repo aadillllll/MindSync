36f85c34f8154971bc154e261808e521c73a56a9
Adil Muhd <adilmuhammeda8@gmail.com>
2026-07-21 21:55:51 +0530

    integrated the render deployed bankends url

--- frontend/lib/features/ai/services/ai_service.dart
+++ frontend/lib/features/ai/services/ai_service.dart
@@ -7,7 +7,7 @@ import '../models/chat_message.dart';
 
 class AIService {
   // Android Emulator
-  static const String baseUrl = "http://10.0.2.2:8000";
+  static const String baseUrl = "https://mindsync-4i76.onrender.com";
 
   // Windows
   // static const String baseUrl = "http://127.0.0.1:8000";
